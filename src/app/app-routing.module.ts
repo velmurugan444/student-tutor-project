c0ff82a74ff049be9e8ed30263f6342ae2664466
velmurugan444 <mvel1620r@gmail.com>
2023-05-03 20:04:30 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { SignupComponent } from './Components/signup/signup.component';
+
+const routes: Routes = [
+   {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
