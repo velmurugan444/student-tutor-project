f1d3e3fe2bcb277bc5ea3371610b7c4c0dab2502
velmurugan444 <mvel1620r@gmail.com>
2023-05-05 19:28:44 +0530

    worked in home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
 import { SignupComponent } from './Components/signup/signup.component';
+import { CourselistComponent } from './Components/courselist/courselist.component';
 
 const routes: Routes = [
    {path:'',component:HomeComponent},
   {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent}
+  {path:'signup',component:SignupComponent},
+  {path:'studenthomepage',component:CourselistComponent}
 ];
 
 @NgModule({
